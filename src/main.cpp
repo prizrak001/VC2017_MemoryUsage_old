66311a1a6953311258867d0e958da730ed647975
PRIZRAK <prizrak@example.com>
2017-04-17 16:36:45 +0400

    Working with std::list

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include <list>
+
+class Aaa {
+    public:
+        int int1;
+        double double1;
+};
+
+using AaaList = std::list<Aaa>;
+
+AaaList aaaList;
+
+void fillList(size_t n) {
+    for (size_t i = 0; i < n; ++i) {
+        aaaList.push_back(Aaa{ 3, 5.7 });
+    }
+}
+
+void fillList() {
+    fillList(300000);
+}
+
+void clearList() {
+    aaaList.clear();
+}
 
 int main() {
-    std::cout << "Hello, ConsoleApp" << std::endl;
+    for (size_t i = 0; i < 5; ++i) {
+        fillList();
+        clearList();
+    }
 
     return EXIT_SUCCESS;
 }
