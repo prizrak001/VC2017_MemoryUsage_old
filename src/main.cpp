0c35dff2bca6116c221175d05cfb03d2347e47ec
PRIZRAK <prizrak@example.com>
2017-04-17 18:05:47 +0400

    Simpla custom allocator

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,55 @@
 #include <iostream>
 #include <list>
 
+#include <memory>
+#include <stdio.h>
+
+// 
+// http://stackoverflow.com/a/12529302/2314910
+// 
+namespace mmap_allocator_namespace
+{
+    template <typename T>
+    class mmap_allocator : public std::allocator<T>
+    {
+    public:
+        typedef size_t size_type;
+        typedef T* pointer;
+        typedef const T* const_pointer;
+
+        template<typename _Tp1>
+        struct rebind
+        {
+            typedef mmap_allocator<_Tp1> other;
+        };
+
+        pointer allocate(size_type n, const void *hint = 0)
+        {
+            fprintf(stderr, "Alloc %d bytes.\n", n * sizeof(T));
+            return std::allocator<T>::allocate(n, hint);
+        }
+
+        void deallocate(pointer p, size_type n)
+        {
+            fprintf(stderr, "Dealloc %d bytes (%p).\n", n * sizeof(T), p);
+            return std::allocator<T>::deallocate(p, n);
+        }
+
+        mmap_allocator() throw() : std::allocator<T>() { fprintf(stderr, "Hello allocator!\n"); }
+        mmap_allocator(const mmap_allocator &a) throw() : std::allocator<T>(a) { }
+        template <class U>
+        mmap_allocator(const mmap_allocator<U> &a) throw() : std::allocator<T>(a) { }
+        ~mmap_allocator() throw() { }
+    };
+}
+
 class Aaa {
     public:
         int int1;
         double double1;
 };
 
-using AaaList = std::list<Aaa>;
+using AaaList = std::list<Aaa, mmap_allocator_namespace::mmap_allocator<Aaa>>;
 
 AaaList aaaList;
 
@@ -18,7 +60,7 @@ void fillList(size_t n) {
 }
 
 void fillList() {
-    fillList(300000);
+    fillList(3);
 }
 
 void clearList() {
